2db1f8e978b59cc13548cdda5bfb271d008de240
Renan Rodrigues <renan.rodrigues@LT-2019-012.local>
2021-08-17 18:31:08 +0100

    UP

---
+++ extension.ps1
@@ -0,0 +1,19 @@
+Param(
+    [string]$dotnetInstallDir = 'C:\dotnet',
+    [string]$dotnetVersion = 'Latest',
+    [string]$dotnetChannel = 'Current'
+)
+
+# Set system path to dotnet installation
+[Environment]::SetEnvironmentVariable("Path", $env:Path + ";$dotnetInstallDir", [EnvironmentVariableTarget]::Machine);
+
+# Force use of TLS12 to download script
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
+
+# Download and run dotnet-install.ps1
+&([scriptblock]::Create((Invoke-WebRequest "https://dot.net/v1/dotnet-install.ps1" -UseBasicParsing))) -Runtime dotnet -Channel $dotnetChannel -Version $dotnetVersion -InstallDir $dotnetInstallDir -NoPath;
+
+Install-WindowsFeature Web-server -IncludeManagementTools
+
+Set-Content -Path “C:\inetpub\wwwroot\Default.htm” -Value $($env:computername)
+
