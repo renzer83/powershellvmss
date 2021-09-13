0898e053ae2d75b669df53645b9e1a0f42d9dc57
Renan Witer Rodrigues <renzer83@gmail.com>
2021-09-13 12:09:42 +0100

    Update removeextension.ps1

--- removeextension.ps1
+++ removeextension.ps1
@@ -1,3 +1,3 @@
 cd C:\azagent\A1
-./config.cmd remove --unattended --auth PAT --token x4oxvozzriktyzl7phm42szdyw64ww2z4jxuf5lwgdid2jh34u4a
+./config.cmd remove --unattended --auth PAT --token qrmmzpbnbwobxgampw7uqbccbj5xxik2iv6wrdcnlyz33krlztea
 Remove-Item -path c:\azagent -recurse -force
