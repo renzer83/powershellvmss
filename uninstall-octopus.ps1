0b0f51e6a632e2ad4a634ea42bd53ed330856674
Renan Witer Rodrigues <renzer83@gmail.com>
2022-08-09 17:11:03 +0100

    Update uninstall-octopus.ps1

--- uninstall-octopus.ps1
+++ uninstall-octopus.ps1
@@ -4,9 +4,11 @@ param (
 
 cd "C:\OctopusDeploy\Tentacle"
 .\Tentacle.exe deregister-worker --server=$SERVER
+Start-Sleep -Seconds 5
 cd C:\Temp
 msiexec.exe /x "Octopus.Tentacle.latest.msi" /QN
 cd
+Start-Sleep -Seconds 5
 Remove-Item -LiteralPath "C:\Octopus" -Force -Recurse
 Remove-Item -LiteralPath "C:\OctopusDeploy" -Force -Recurse  
 sc.exe delete "OctopusDeploy Tentacle"
