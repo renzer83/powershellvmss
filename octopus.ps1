ead120815e356ac99700910000e0ba71960e9ff0
Renan Witer Rodrigues <renzer83@gmail.com>
2022-08-12 11:43:44 +0100

    Update octopus.ps1

--- octopus.ps1
+++ octopus.ps1
@@ -1,4 +1,4 @@
-param (
+   param (
     $APIKEY,
     $NAME,
     $SERVER,
@@ -6,23 +6,26 @@ param (
     $SPACE,
     $ENVIRONMENT,
     $ROLE,
-    $POLICY
+    $POLICY,
+    $serverThumbprint
 )
 
-## Download client Octopus
+## Download client Octopus and install
 $clientURL = "https://octopus.com/downloads/latest/WindowsX64/OctopusTentacle"
 $clientDestination = "C:\Temp\Octopus.Tentacle.latest.msi"
+$serverThumbprint = $serverThumbprint
 Invoke-WebRequest $clientURL -OutFile $clientDestination
 cd C:\Temp
 msiexec INSTALLLOCATION="C:\OctopusDeploy\Tentacle" /i Octopus.Tentacle.latest.msi /quiet
-
-#msiexec.exe /x "Octopus.Tentacle.latest.msi"
+$ipv4 = Get-NetIPAddress -AddressFamily IPv4 -InterfaceIndex $(Get-NetConnectionProfile | Select-Object -ExpandProperty InterfaceIndex) | Select-Object -ExpandProperty IPAddress
 Start-Sleep -Seconds 5
 cd "C:\OctopusDeploy\Tentacle"
 # Configure Tentacle Octopus
-.\Tentacle.exe create-instance --instance "Tentacle" --config "C:\Octopus\Tentacle.config" --console
-.\Tentacle.exe new-certificate --instance "Tentacle" --if-blank --console
-.\Tentacle.exe configure --instance "Tentacle" --reset-trust --console
-.\Tentacle.exe configure --instance "Tentacle" --app "C:\Octopus\Applications" --noListen "True" --console 
-.\Tentacle.exe register-with --instance "Tentacle" --server $SERVER --name $NAME --comms-style "TentacleActive" --server-comms-port $PORT --apiKey $APIKEY --space $SPACE --force --environment $ENVIRONMENT --role $ROLE --policy $POLICY --console
-.\Tentacle.exe service --instance "Tentacle" --install --stop --start --console
+./Tentacle.exe create-instance --instance "Tentacle" --config "C:\Octopus\Tentacle.config" --console
+./Tentacle.exe new-certificate --instance "Tentacle" --if-blank --console
+./Tentacle.exe configure --instance "Tentacle" --reset-trust --console
+./Tentacle.exe configure --instance "Tentacle" --home "C:\Octopus" --app "C:\Octopus\Applications" --port $PORT --console
+./Tentacle.exe configure --instance "Tentacle" --trust $serverThumbprint --console
+netsh advfirewall firewall add rule "name=Octopus Deploy Tentacle" dir=in action=allow protocol=TCP localport=$PORT
+./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --publicHostName=$ipv4 --space $SPACE --role $ROLE --environment $ENVIRONMENT --comms-style TentaclePassive --console
+./Tentacle.exe service --instance "Tentacle" --install --start --console
