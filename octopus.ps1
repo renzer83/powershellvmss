ea1916ac6af3b4153d9f13f3810dac0b0642d398
Renan Witer Rodrigues <renzer83@gmail.com>
2022-09-07 10:57:59 +0100

    Update octopus.ps1

--- octopus.ps1
+++ octopus.ps1
@@ -6,6 +6,7 @@
     $SPACE,
     $ENVIRONMENT,
     $ROLE,
+    $ROLE02,
     $POLICY,
     $serverThumbprint
 )
@@ -29,5 +30,5 @@ $ipv4 = Get-NetIPAddress -AddressFamily IPv4 -InterfaceIndex $(Get-NetConnection
 cd "C:\Program Files\Octopus Deploy\Tentacle" 
 .\Tentacle.exe deregister-from --server $SERVER --apiKey $APIKEY --instance=$NAME  --space=$SPACE
 Start-Sleep -Seconds 2
-./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --publicHostName=$ipv4 --space $SPACE --role $ROLE --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
+./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --publicHostName=$ipv4 --space $SPACE --role $ROLE --role $ROLE02 --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
 #./Tentacle.exe service --instance "Tentacle" --install --start --console
