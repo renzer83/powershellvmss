80b57e92e00d5728cc8e1250a61c7558063bbf6e
Renan Witer Rodrigues <renzer83@gmail.com>
2022-09-13 14:02:16 +0100

    Update octopus.ps1

--- octopus.ps1
+++ octopus.ps1
@@ -1,4 +1,4 @@
-   param (
+param (
     $APIKEY,
     $NAME,
     $SERVER,
@@ -6,29 +6,17 @@
     $SPACE,
     $ENVIRONMENT,
     $ROLE,
-    $ROLE02,
     $POLICY,
     $serverThumbprint
 )
 
-## Download client Octopus and install
-# $clientURL = "https://octopus.com/downloads/latest/WindowsX64/OctopusTentacle"
-# $clientDestination = "C:\Temp\Octopus.Tentacle.latest.msi"
-# $serverThumbprint = $serverThumbprint
-# Invoke-WebRequest $clientURL -OutFile $clientDestination
-# cd C:\Temp
-# msiexec INSTALLLOCATION="C:\OctopusDeploy\Tentacle" /i Octopus.Tentacle.latest.msi /quiet
 $ipv4 = Get-NetIPAddress -AddressFamily IPv4 -InterfaceIndex $(Get-NetConnectionProfile | Select-Object -ExpandProperty InterfaceIndex) | Select-Object -ExpandProperty IPAddress
-#cd "C:\OctopusDeploy\Tentacle"
-# Configure Tentacle Octopus
-#./Tentacle.exe create-instance --instance "Tentacle" --config "C:\Octopus\Tentacle.config" --console
-#./Tentacle.exe new-certificate --instance "Tentacle" --if-blank --console
-#./Tentacle.exe configure --instance "Tentacle" --reset-trust --console
-#./Tentacle.exe configure --instance "Tentacle" --home "C:\Octopus" --app "C:\Octopus\Applications" --port $PORT --console
-#./Tentacle.exe configure --instance "Tentacle" --trust $serverThumbprint --console
-#netsh advfirewall firewall add rule "name=Octopus Deploy Tentacle" dir=in action=allow protocol=TCP localport=$PORT
 cd "C:\Program Files\Octopus Deploy\Tentacle" 
 .\Tentacle.exe deregister-from --server $SERVER --apiKey $APIKEY --instance=$NAME  --space=$SPACE
 Start-Sleep -Seconds 2
-./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --publicHostName=$ipv4 --space $SPACE --role $ROLE --role $ROLE02 --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
-#./Tentacle.exe service --instance "Tentacle" --install --start --console
+New-Object -TypeName System.Collections.ArrayList
+$arrlist = [System.Collections.Arraylist]@()
+For ($i=0; $i -lt $ROLE.Length; $i++) {
+    $arrlist.Add("--role "+$ROLE[$i])
+    }
+./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --publicHostName=$ipv4 --space $SPACE $arrlist --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
