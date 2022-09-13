c58a6bd821554ea326d56be6607f33053660f0e9
Renan Witer Rodrigues <renzer83@gmail.com>
2022-09-13 18:01:17 +0100

    Update octopus.ps1

--- octopus.ps1
+++ octopus.ps1
@@ -14,19 +14,5 @@ $ipv4 = Get-NetIPAddress -AddressFamily IPv4 -InterfaceIndex $(Get-NetConnection
 cd "C:\Program Files\Octopus Deploy\Tentacle" 
 .\Tentacle.exe deregister-from --server $SERVER --apiKey $APIKEY --instance=$NAME  --space=$SPACE
 Start-Sleep -Seconds 2
-New-Object -TypeName System.Collections.ArrayList
-$arrlist = [System.Collections.Arraylist]@($ROLE)
-
-if ($arrlist.Length -eq 4) {
-    ./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --publicHostName=$ipv4 --space $SPACE --role $arrlist[0] --role $arrlist[1] --role $arrlist[2] --role $arrlist[3] --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
-}
-elseif ($arrlist.Length -eq 3) {
-    ./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --publicHostName=$ipv4 --space $SPACE --role $arrlist[0] --role $arrlist[1] --role $arrlist[2] --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
-}
-elseif ($arrlist.Length -eq 2) {
-    ./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --publicHostName=$ipv4 --space $SPACE --role $arrlist[0] --role $arrlist[1] --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
-}
-elseif ($arrlist.Length -eq 1) {
-    ./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --publicHostName=$ipv4 --space $SPACE --role $arrlist[0] --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
-} 
+./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --publicHostName=$ipv4 --space $SPACE $ROLE --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
 
