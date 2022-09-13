6a586cb303966f4e5ea9de812f8de4f7b150b7ee
Renan Witer Rodrigues <renzer83@gmail.com>
2022-09-13 17:37:57 +0100

    Update octopus.ps1

--- octopus.ps1
+++ octopus.ps1
@@ -18,7 +18,7 @@ New-Object -TypeName System.Collections.ArrayList
 $arrlist = [System.Collections.Arraylist]@($ROLE)
 
 if ($arrlist.Length -eq 4) {
-    ./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --publicHostName=$ipv4 --space $SPACE --role $ROLE[0] --role $arrlist[1] --role $arrlist[2] --role $arrlist[3] --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
+    ./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --publicHostName=$ipv4 --space $SPACE --role $arrlist[0] --role $arrlist[1] --role $arrlist[2] --role $arrlist[3] --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
 }
 elseif ($arrlist.Length -eq 3) {
     ./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --publicHostName=$ipv4 --space $SPACE --role $arrlist[0] --role $arrlist[1] --role $arrlist[2] --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
