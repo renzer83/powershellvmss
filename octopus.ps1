cfea0d42b27ba1f5c8986bb0b3a1978caea082d1
Renan Witer Rodrigues <renzer83@gmail.com>
2022-09-14 13:11:09 +0100

    Update octopus.ps1

--- octopus.ps1
+++ octopus.ps1
@@ -15,5 +15,12 @@ cd "C:\Program Files\Octopus Deploy\Tentacle"
 .\Tentacle.exe deregister-from --server $SERVER --apiKey $APIKEY --instance=$NAME  --space=$SPACE
 Start-Sleep -Seconds 2
 $array = $ROLE.split(",")
-./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --publicHostName=$ipv4 --space $SPACE --role $array[0] --role $array[1] --role $array[2] --role $array[3] --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
+New-Object -TypeName System.Collections.ArrayList
+$arrayrole = [System.Collections.Arraylist]@()
+For ($i=0; $i -lt $array.Length; $i++) {
+    $arrayrole.Add("--role "+$array[$i])
+    }
+[String[]]$tostringrole = $arrayrole
+./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --publicHostName=$ipv4 --space $SPACE $tostringrole --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
 #./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --publicHostName=$ipv4 --space $SPACE $ROLE --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
+# ./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --publicHostName=$ipv4 --space $SPACE --role $array[0] --role $array[1] --role $array[2] --role $array[3] --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
