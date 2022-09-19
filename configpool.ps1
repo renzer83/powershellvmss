490a152d7042375827e8d03cbcd1b19471b77352
Renan Witer Rodrigues <renzer83@gmail.com>
2022-09-19 10:57:22 +0100

    Update configpool.ps1

--- configpool.ps1
+++ configpool.ps1
@@ -9,5 +9,21 @@ $identity = @{  `
     username="$USERNAME"; `
     password="$PASS" `
 }
-Set-ItemProperty -Path "IIS:\AppPools\$POOLNAME" -name "processModel" -value $identity
+
+$array = $POOLNAME.split(",")
+if ($array.count -eq 4){
+    Set-ItemProperty -Path "IIS:\AppPools\$array[0]" -name "processModel" -value $identity
+    Set-ItemProperty -Path "IIS:\AppPools\$array[1]" -name "processModel" -value $identity
+    Set-ItemProperty -Path "IIS:\AppPools\$array[2]" -name "processModel" -value $identity
+    Start-WebAppPool -Name $array[0]
+    Start-WebAppPool -Name $array[1]
+    Start-WebAppPool -Name $array[2]
+}
+elseif ($array.count -eq 2){
+    Set-ItemProperty -Path "IIS:\AppPools\$array[0]" -name "processModel" -value $identity
+    ./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --publicHostName=$ipv4 --space $SPACE --role $array[0] --role $array[1] --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
+}
  
+Start-WebAppPool -Name 360imprimir-beta-BR
+Start-WebAppPool -Name 360imprimir-beta-MX
+Start-WebAppPool -Name 360imprimir-beta-PT
