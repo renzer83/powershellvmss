fc42bb26151c9334f5bb69ce037336f0615d3dba
Renan Witer Rodrigues <renzer83@gmail.com>
2022-12-13 10:10:07 +0000

    Update octopus.ps1
    Multiples tentacle

--- octopus.ps1
+++ octopus.ps1
@@ -12,14 +12,18 @@ param (
 
 $ipv4 = Get-NetIPAddress -AddressFamily IPv4 -InterfaceIndex $(Get-NetConnectionProfile | Select-Object -ExpandProperty InterfaceIndex) | Select-Object -ExpandProperty IPAddress
 cd "C:\Program Files\Octopus Deploy\Tentacle" 
-.\Tentacle.exe deregister-from --server $SERVER --apiKey $APIKEY --instance=$NAME  --space=$SPACE
-Start-Sleep -Seconds 2
 $array = $ROLE.split(",")
-if ($array.count -eq 4){
-    ./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --publicHostName=$ipv4 --space $SPACE --role $array[0] --role $array[1] --role $array[2] --role $array[3] --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
+if ($ENVIRONMENT -eq "05.PRD NA"){
+    ./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --name "$NAME-MX" --publicHostName=$ipv4 --space $SPACE --role $array[0] --role $array[1] --role $array[2] --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
 }
-elseif ($array.count -eq 2){
-    ./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --publicHostName=$ipv4 --space $SPACE --role $array[0] --role $array[1] --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
+elseif ($ENVIRONMENT -eq "06.PRD BR"){
+    ./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --name "$NAME-BR" --publicHostName=$ipv4 --space $SPACE --role $array[0] --role $array[1] --role $array[2] --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
 }
-#./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --publicHostName=$ipv4 --space $SPACE $ROLE --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
-
+elseif ($ENVIRONMENT -eq "07.PRD EU"){
+    ./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --name "$NAME-EU"  --publicHostName=$ipv4 --space $SPACE --role $array[0] --role $array[1] --role $array[2] --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
+}
+else{
+    ./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --name "$NAME-MX" --publicHostName=$ipv4 --space $SPACE --role $array[0] --role "MX" --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
+    ./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --name "$NAME-BR" --publicHostName=$ipv4 --space $SPACE --role $array[1] --role "BR" --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
+    ./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --name "$NAME-EU" --publicHostName=$ipv4 --space $SPACE --role $array[2] --role "PT" --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
+}
