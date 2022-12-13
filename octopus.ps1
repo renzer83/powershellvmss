9447919d13733f3d002205399ccac72bc2a1d374
Renan Witer Rodrigues <renzer83@gmail.com>
2022-12-13 17:20:20 +0000

    Update octopus.ps1

--- octopus.ps1
+++ octopus.ps1
@@ -23,7 +23,7 @@ elseif ($ENVIRONMENT -eq "07.PRD EU"){
     ./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --name "$NAME-EU"  --publicHostName=$ipv4 --space $SPACE --role $array[0] --role $array[1] --role $array[2] --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
 }
 else{
-    ./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --name "$NAME-MX" --publicHostName=$ipv4 --space $SPACE --role $array[0] --role "MX" --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
-    ./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --name "$NAME-BR" --publicHostName=$ipv4 --space $SPACE --role $array[1] --role "BR" --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
-    ./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --name "$NAME-EU" --publicHostName=$ipv4 --space $SPACE --role $array[2] --role "PT" --role $array[3] --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
+    ./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --name "$NAME-MX" --publicHostName=$ipv4 --space $SPACE --role "MX" --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
+    ./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --name "$NAME-BR" --publicHostName=$ipv4 --space $SPACE --role "BR" --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
+    ./Tentacle.exe register-with --instance "Tentacle" --server $SERVER --apiKey $APIKEY --name "$NAME-EU" --publicHostName=$ipv4 --space $SPACE --role "PT" --role $array[3] --environment $ENVIRONMENT --comms-style TentaclePassive --force --console
 }
