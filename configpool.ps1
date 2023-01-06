ce7998b76d466935d1e95bae38036e58f05d2db8
Renan Witer Rodrigues <renzer83@gmail.com>
2023-01-06 15:18:06 +0000

    Update configpool.ps1
    Install cert bind

--- configpool.ps1
+++ configpool.ps1
@@ -1,7 +1,10 @@
  param (
     $USERNAME,
     $PASS,
-    $POOLNAME
+    $POOLNAME,
+    $URL,
+    $CERT,
+    $NA
 )
 Import-Module WebAdministration
 $identity = @{  `
@@ -27,3 +30,14 @@ for ($i=0; $i -lt $array.length; $i++){
    Start-WebAppPool -Name $pool
 
 } 
+
+$zipFile = $URL
+Invoke-WebRequest -Uri $zipFile -OutFile 'C:\Temp\website.pfx'
+$Path='C:\Temp\website.pfx'
+$Secure_String_Pwd = ConvertTo-SecureString $CERT -AsPlainText -Force
+Import-PfxCertificate -FilePath $Path -Password $Secure_String_Pwd -CertStoreLocation "Cert:\LocalMachine\My"
+$iisCert = Get-ChildItem -Path "Cert:\LocalMachine\MY" | where{$_.Subject -eq "CN=beta.360imprimir.pt"}
+$applicationId = [Guid]::NewGuid().ToString("B") 
+netsh http add sslcert ipport=0.0.0.0:443 certhash=$($iisCert.Thumbprint) appid=$applicationId
+New-WebBinding -Name $NA -IP "*" -Port 443 -Protocol https  
+
